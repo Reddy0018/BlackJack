5ecf48f6a5bcc2dad2e5fd3ce8788cf05ae604b8
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-09-14 23:19:28 -0400

    Added user Entity
    Added user Entity

---
+++ demo/src/main/java/com/blackjack/game/user/User.java
@@ -0,0 +1,78 @@
+package com.blackjack.game.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+//import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @Id
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false,unique = true)
+    private String email;
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+    /**@JsonIgnore
+    @Column
+    private String salt;*/
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
