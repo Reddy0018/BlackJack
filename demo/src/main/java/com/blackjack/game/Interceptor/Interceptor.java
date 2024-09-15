b8cdc564bf580a286beae256ea460d90eacf2ed8
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-09-15 16:32:54 -0400

    Removed Unwanted Imports
    Removed Unwanted Imports

--- demo/src/main/java/com/blackjack/game/Interceptor/Interceptor.java
+++ demo/src/main/java/com/blackjack/game/Interceptor/Interceptor.java
@@ -5,13 +5,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
-import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.io.IOException;
-import java.util.HashMap;
 
 @Component
 public class Interceptor implements HandlerInterceptor {
@@ -21,7 +17,6 @@ public class Interceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
         System.out.println("Inside Pre Handle request: "+ request);
-        //return false;
         if(null== UserService.getActiveUserName()){
             response.setStatus(HttpServletResponse.SC_FORBIDDEN);
             response.setCharacterEncoding("UTF-8");
@@ -35,14 +30,12 @@ public class Interceptor implements HandlerInterceptor {
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("Inside Post Handle");
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response,
                                 Object handler, Exception ex) throws Exception {
-        System.out.println("Inside After Completion!");
         HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
     }
 
--- demo/src/main/java/com/blackjack/game/user/User.java
+++ demo/src/main/java/com/blackjack/game/user/User.java
@@ -2,7 +2,6 @@ package com.blackjack.game.user;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
-//import javax.persistence.*;
 
 @Entity
 @Table(name = "users")
