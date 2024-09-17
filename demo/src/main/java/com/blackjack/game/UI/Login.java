49265b908e92ba406434f735b57aec1726795b4c
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-09-17 02:13:12 -0400

    Added UI Code for Login flow using JFrame Lib
    Added UI Code for Login flow using JFrame Lib

---
+++ demo/src/main/java/com/blackjack/game/UI/Login.java
@@ -0,0 +1,208 @@
+package com.blackjack.game.UI;
+
+import com.blackjack.game.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class Login implements ActionListener {
+    private static JLabel userLabel, passwordLabel, success, firstNameLabel, lastNameLabel;
+    private static JButton loginButton, signupButton, signUp, switchToLoginButton;
+    private static JPasswordField passwordBox;
+    private static JTextField userTextBox, firstNameTextBox, lastNameTextBox;
+
+    private static JFrame jFrame=null;
+    private static JPanel jPanel=null;
+
+    @Autowired
+    UserService userService;
+
+    /**public static void main(String[] args) {
+        Login login = new Login();
+        login.createLoginScreen();
+    }*/
+
+    public void createLoginScreen() {
+        if(null!=jFrame){
+            jFrame.dispose();
+        }
+        jFrame = new JFrame();
+        jPanel = new JPanel();
+
+        //Frame Creation with specific bounds
+        jFrame.setSize(350,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(jPanel);
+        jPanel.setLayout(null);
+
+        // User related UI Code
+        userLabel = new JLabel("User");
+        userLabel.setBounds(10,20,80,25);
+        jPanel.add(userLabel);
+        userTextBox = new JTextField(20);
+        userTextBox.setBounds(100,20,200,25);
+        jPanel.add(userTextBox);
+
+        // Password related UI Code
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10,50,80,25);
+        jPanel.add(passwordLabel);
+        passwordBox = new JPasswordField();
+        passwordBox.setBounds(100,50,200,25);
+        jPanel.add(passwordBox);
+
+        loginButton = new JButton("Login");
+        loginButton.setBounds(10,80,80,25);
+        loginButton.addActionListener(new Login());
+        jPanel.add(loginButton);
+
+        signUp = new JButton("SignUp");
+        signUp.setBounds(90,80,80,25);
+        signUp.addActionListener(new Login());
+        jPanel.add(signUp);
+
+        success = new JLabel("");
+        success.setBounds(10,110,300,25);
+        jPanel.add(success);
+        jFrame.setVisible(true);
+    }
+
+    public void createSignupScreen() {
+        jFrame.dispose();
+
+        jFrame = new JFrame();
+        jPanel = new JPanel();
+        jFrame.setSize(350,400);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(jPanel);
+        jPanel.setLayout(null);
+
+        // User related UI Code
+        userLabel = new JLabel("Email");
+        userLabel.setBounds(10, 20, 80, 25);
+        jPanel.add(userLabel);
+        userTextBox = new JTextField(20);
+        userTextBox.setBounds(100, 20, 200, 25);
+        jPanel.add(userTextBox);
+
+        firstNameLabel = new JLabel("First Name");
+        firstNameLabel.setBounds(10, 50, 80, 25);
+        jPanel.add(firstNameLabel);
+        firstNameTextBox = new JTextField(20);
+        firstNameTextBox.setBounds(100, 50, 200, 25);
+        jPanel.add(firstNameTextBox);
+
+        lastNameLabel = new JLabel("Last Name");
+        lastNameLabel.setBounds(10, 80, 80, 25);
+        jPanel.add(lastNameLabel);
+        lastNameTextBox = new JTextField(20);
+        lastNameTextBox.setBounds(100, 80, 200, 25);
+        jPanel.add(lastNameTextBox);
+
+        // Password related UI Code
+        passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 110, 80, 25);
+        jPanel.add(passwordLabel);
+        passwordBox = new JPasswordField();
+        passwordBox.setBounds(100, 110, 200, 25);
+        jPanel.add(passwordBox);
+
+        // Signup Button
+        signupButton = new JButton("Sign Up");
+        signupButton.setBounds(5, 140, 80, 25);
+        signupButton.addActionListener(this);
+        signupButton.setActionCommand("Sign Up");
+        jPanel.add(signupButton);
+
+        // Switch back to Login Screen
+        switchToLoginButton = new JButton("Login");
+        switchToLoginButton.setBounds(80, 140, 80, 25);
+        switchToLoginButton.addActionListener(this);
+        switchToLoginButton.setActionCommand("switchToLogin");
+        jPanel.add(switchToLoginButton);
+
+        success = new JLabel("");
+        success.setBounds(10, 200, 500, 25);
+        jPanel.add(success);
+        jFrame.setVisible(true);
+
+        jFrame.revalidate();
+        jFrame.repaint();
+        jFrame.setVisible(true);
+    }
+
+    private boolean checkEmpty(Map<String,String> map){
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String s2 = entry.getValue();
+            if (s2.isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println(e.getActionCommand());
+        String action = e.getActionCommand();
+        switch (action) {
+            case "Login":
+                Map<String,String> loginRequestMap = new HashMap<>();
+                loginRequestMap.put("email",userTextBox.getText());
+                loginRequestMap.put("password",passwordBox.getText());
+                try {
+                    if(checkEmpty(loginRequestMap)){
+                        success.setText("Fields cannot be empty!");
+                        break;
+                    }
+                    /**if (nameField.getText().isEmpty() || emailField.getText().isEmpty()) {
+                        JOptionPane.showMessageDialog(MandatoryFieldsExample.this,
+                                "Please fill in all required fields.", "Error", JOptionPane.ERROR_MESSAGE);
+                    }*/
+                    BeanProvider.autowire(this);
+                    boolean loginStatus = userService.validateLoginDetails(loginRequestMap);
+                    if(loginStatus){
+                        success.setText("Login Success");
+                    }else {
+                        success.setText("User Not Valid");
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    success.setText(ex.getMessage());
+                }
+                break;
+            case "switchToLogin":
+                createLoginScreen();
+                break;
+            case "Sign Up":
+                Map<String,String> signUpRequestMap = new HashMap<>();
+                signUpRequestMap.put("email",userTextBox.getText());
+                signUpRequestMap.put("password",passwordBox.getText());
+                signUpRequestMap.put("firstName",firstNameTextBox.getText());
+                signUpRequestMap.put("lastName",lastNameTextBox.getText());
+                try {
+                    if(checkEmpty(signUpRequestMap)){
+                        success.setText("Fields cannot be empty!");
+                        break;
+                    }
+                    BeanProvider.autowire(this);
+                    userService.validateSignUp(signUpRequestMap);
+                    success.setText("Sign UP Success");
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                    success.setText(ex.getMessage());
+                }
+                break;
+            case "SignUp":
+                createSignupScreen();
+                break;
+            default:
+                break;
+        }
+    }
+}
