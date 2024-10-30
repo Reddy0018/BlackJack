5c3a42ce95f69d7753b39802f43704b02a2efdfd
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-10-30 19:21:17 -0400

    Added Lombok Setters and getters
    Added Lombok Setters and getters

--- demo/src/main/java/com/blackjack/game/user/User.java
+++ demo/src/main/java/com/blackjack/game/user/User.java
@@ -3,12 +3,16 @@ package com.blackjack.game.user;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "users")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
@@ -39,64 +43,4 @@ public class User {
     private Integer totalWins;
     @Column(name = "total_losses",nullable = true)
     private Integer totalLosses;
-
-    /**@JsonIgnore
-    @Column
-    private String salt;*/
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getTotalWins() {
-        return totalWins;
-    }
-
-    public void setTotalWins(int totalWins) {
-        this.totalWins = totalWins;
-    }
-
-    public int getTotalLosses() {
-        return totalLosses;
-    }
-
-    public void setTotalLosses(int totalLosses) {
-        this.totalLosses = totalLosses;
-    }
 }
