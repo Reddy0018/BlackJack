fdd1e291612ab2ce4b7a4992d7692aa4fe4e0bd7
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-11-01 01:36:52 -0400

    Fixed Junit Test Issue
    Fixed Junit Test Issue

--- demo/src/test/java/com/blackjack/game/user/UserRepositoryTest.java
+++ demo/src/test/java/com/blackjack/game/user/UserRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -12,7 +13,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest
 class UserRepositoryTest {
 
-    @Autowired
+    @MockBean
     private UserRepository userRepository;
 
     @Test
