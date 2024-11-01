5960b16a5eb8b6854532c96fffa4a024a6f2aa51
Sumanth Reddy Bekkem <sxb1454@case.edu>
2024-11-01 01:44:24 -0400

    code refactor
    code refactor

--- demo/src/test/java/com/blackjack/game/user/UserRepositoryTest.java
+++ demo/src/test/java/com/blackjack/game/user/UserRepositoryTest.java
@@ -1,14 +1,11 @@
 package com.blackjack.game.user;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class UserRepositoryTest {
