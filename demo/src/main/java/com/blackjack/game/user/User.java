b5e20ae417a05a332d056f89e4ee4fe8948388ea
WSFromCWRU <was103@case.edu>
2024-11-14 22:24:22 -0500

    some test cases run

--- demo/src/main/java/com/blackjack/game/user/User.java
+++ demo/src/main/java/com/blackjack/game/user/User.java
@@ -10,6 +10,7 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@Data
 public class User {
     @SequenceGenerator(name = "user_seq", sequenceName = "user_seq")
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
